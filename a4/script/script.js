96d21353a67971aa8ef77031314a63b72f91ddbf
김나연 <kny8664@naver.com>
2024-10-29 10:40:20 +0900

    a4

---
+++ a4/script/script.js
@@ -0,0 +1,19 @@
+$('.main-menu').mouseenter(function () {
+    $('.sub-menu').stop().slideDown()
+    $('header').addClass('on')
+})
+$('.main-menu').mouseleave(function () {
+    $('.sub-menu').stop().slideUp()
+    $('header').removeClass('on')
+})
+
+$('slide').eq(0).siblings().hide()
+let idx = 0
+setInterval(function () {
+    idx++
+    if (idx === 3) {
+        idx = 0
+    }
+    $('.slide').fadeOut()
+    $('.slide').eq(idx).fadeIn()
+},3000)
